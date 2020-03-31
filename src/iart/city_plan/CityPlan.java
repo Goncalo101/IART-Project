e88420cbb95ad45510a7deb887b8c6e2add8032d
Andre Mori <andremori16@gmail.com>
2020-03-31 23:35:00 +0100

    updated main class

--- src/iart/city_plan/CityPlan.java
+++ src/iart/city_plan/CityPlan.java
@@ -8,15 +8,27 @@ import java.io.IOException;
 
 public class CityPlan {
     public static void main(String[] args) throws IOException {
-        String inputFileName = args[0];
+        String inputFileName = args[1];
         InputProcessor ip = new InputProcessor(inputFileName);
 
-        Strategy strategy = new SimulatedAnnealingStrategy(ip.getCity());
+        Strategy strategy = null;
+
+        if(args[0].equals("hillClimbing")){
+            strategy = new HillClimbingStrategy(ip.getCity());
+        } else if(args[0].equals("simulatedAnnealing")){
+            strategy = new SimulatedAnnealingStrategy(ip.getCity());
+        } else if(args[0].equals("genetic")){
+            strategy = new GeneticStrategy(ip.getCity());
+        } else{
+            System.out.println("Invalid strategy. Options: hillClimbing, simulatedAnnealing or genetic");
+            System.exit(-1);
+        }
+
         Solution solution = strategy.solve(ip.getBuildingProjects());
 
         System.out.println(solution);
 
-        String outputFileName = args[1];
+        String outputFileName = args[2];
         OutputProcessor op = new OutputProcessor(outputFileName, solution);
         op.writeSolution(solution);
     }
